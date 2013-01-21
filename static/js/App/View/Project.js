e19145615304b65695c22b5a51873a42eeb7bb87
Craig <decoy9697@gmail.com>
2013-01-22 01:17:01 +1300

    Animating in tech logos for each project. Includes source images.

--- static/js/App/View/Project.js
+++ static/js/App/View/Project.js
@@ -6,13 +6,25 @@ App.module('View', function(View, App, Backbone, Marionette, $, _) {
 
         initialize: function() {},
 
+        animate: function() {
+
+            this.$el.find('.transformed').removeClass('transformed');
+            this.$el.find('.techList li').each(function(i, li) {
+                _.log(i, li);
+                var time = i * 150;
+                setTimeout(function() {
+                    $(li).removeClass('techTransformed');
+                }, time);
+            });
+        },
+
         render: function() {
             var json = this.model.toJSON();
             var html = App.Tmpl[this.template](json);
             var self = this;
             this.$el.html(html);
             _.defer(function() {
-                self.$el.find('.transformed').removeClass('transformed');
+                self.animate();
             });
         }
     });
