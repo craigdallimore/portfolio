6a1ac6e9269748d651e0121ce9f5cfda7ff9e46b
Craig <decoy9697@gmail.com>
2013-01-25 10:03:47 +1300

    Nojs HTML for About page

--- routes/about.js
+++ routes/about.js
@@ -2,23 +2,27 @@ exports.init = function(app, db, q) {
 
     app.get('/about/', function(req, res) {
 
+        var dfdTech = q.defer();
         var dfdNetworks = q.defer();
         var dfdBooks = q.defer();
         var dfdProfile = q.defer();
 
-        var render = function(networks, books, profile) {
+        var render = function(tech, networks, books, profile) {
             res.render('about.jade', {
+                technologies: tech,
                 networks: networks,
                 books: books,
                 profile: profile
             });
         };
 
+        db.findAll('technologies', dfdTech.resolve);
         db.findAll('networks', dfdNetworks.resolve);
         db.findAll('books', dfdBooks.resolve);
         db.findOne('profiles', 'id', 1, dfdProfile.resolve);
 
         q.all([
+            dfdTech.promise,
             dfdNetworks.promise,
             dfdBooks.promise,
             dfdProfile.promise
--- static/js/App/View/Profile.js
+++ static/js/App/View/Profile.js
@@ -3,10 +3,28 @@ App.module('View', function(View, App, Backbone, Marionette, $, _) {
     View.Profile = Marionette.View.extend({
         tagName: 'section',
         template: 'Profile',
+        initialize: function() {
+
+        },
+
+        animateAnchors: function() {
+            var $anchors = this.$el.find('a');
+            _.log($anchors);
+            $anchors.each(function($anchor) {
+                $anchor.removeClass('transformedAnchor');
+            });
+        },
+
         render: function() {
             var json = this.model.toJSON();
             var html = App.Tmpl[this.template](json);
+            var self = this;
+
             this.$el.html(html);
+
+            _.defer(function() {
+                self.animateAnchors();
+            });
         }
     });
 
