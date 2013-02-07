8bc87324c3b84e366cd827f3ff3432b7ec870cb3
Craig <decoy9697@gmail.com>
2013-02-08 00:05:17 +1300

    Seems like I'm going back and forth on what will 302

--- static/js/App/Collection/Project.js
+++ static/js/App/Collection/Project.js
@@ -3,7 +3,7 @@ App.module('Collection', function(Collection, App, Backbone, Marionette, $, _) {
     Collection.Project = Backbone.Collection.extend({
         model: App.Model.Project,
         url: function() {
-            return '/api/projects/';
+            return '/api/projects';
         },
         comparator: function(project) {
             date = project.get('startDate').replace(/-/g, '');
--- static/js/App/Collection/Tech.js
+++ static/js/App/Collection/Tech.js
@@ -3,7 +3,7 @@ App.module('Collection', function(Collection, App, Backbone, Marionette, $, _) {
     Collection.Tech = Backbone.Collection.extend({
         model: App.Model.Tech,
         url: function() {
-            return '/api/tech/';
+            return '/api/tech';
         },
         comparator: function(tech) {
           var affinity = tech.get('affinity');
